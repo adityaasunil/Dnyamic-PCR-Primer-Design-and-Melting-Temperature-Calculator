a89e5ce0f227ddbc04cff0b72a2fcee3b3dc43b6
Aditya Sunil Nair <adusunil2460@gmail.com>
2026-06-06 05:20:44 +0100

    Added a DNA sequence generator and implemented it into the Agent class giving the user a choice to use a DNA sequence generator in case if he wants to try out an example

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
+import random
+
+def sequence_generator(desired_length: int) -> str:
+
+    if desired_length == 0:
+        raise ValueError("The length of the sequence can't be 0bp.")
+
+    nucleotides = ['c', 'g', 'a', 't']
+    seq = ''
+    for i in range(desired_length):
+        seq += nucleotides[random.randint(0, len(nucleotides)-1)]
+    return seq.upper()
+
 class Agent:
-    def __init__(self, sequence: str):
-        self.seq = sequence.upper()
+    def __init__(self,use_sequence_generator:bool, desired_primer_length:int, seq_length=0, sequence = ''):
+
+        self.use_generator = use_sequence_generator
+
+        if self.use_generator == True:
+            self.seq = sequence_generator(seq_length)
+        else:
+            self.seq = sequence
+
+        self.length = desired_primer_length
     
     def __str__(self):
-        return f'The DNA sequence is: {self.seq}'
+        return f'The DNA sequence is: 5\'-{self.seq}-\'3\nThe desired primer length: {round(self.length/1000, 1)}kb'
     
 if __name__ == '__main__':
-    ob = Agent('cgagcgct')
+    ob = Agent(True, 1500, 3000)
     print(ob)
